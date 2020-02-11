de835eeae29b32bbd52743f53bf66186bbc3a07f
ssuchir <ssuchir@gmail.com>
2020-02-10 19:40:05 -0500

    Added video player and styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
 
-  <div>
-    <img src={video.snippet.thumbnails.medium.url}/>
-    {video.snippet.title}
+  <div onClick={() => onVideoSelect(video)} className="video-item item">
+    <img alt={video.snippet.title }className="ui image" src={video.snippet.thumbnails.medium.url}/>
+    <div className="content">
+      <div className="header">{video.snippet.title}</div>
+    </div>
   </div>
 );
 };
