2a04b998e61dc2ce679e57f36b492aa42d24b6cb
ssuchir <ssuchir@gmail.com>
2020-02-11 21:03:06 -0500

    Updates

---
+++ precache-manifest.71d616028fea6ae270ef90406a5d4764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7930bf30dfbd69f5d0f16b77e7318043",
+    "url": "/React-Youtube/index.html"
+  },
+  {
+    "revision": "75fcf4d9ba5189c6bfb8",
+    "url": "/React-Youtube/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "5700e5aa49afebc52af2",
+    "url": "/React-Youtube/static/js/2.b23e50d9.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/React-Youtube/static/js/2.b23e50d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fcf4d9ba5189c6bfb8",
+    "url": "/React-Youtube/static/js/main.1dd052ba.chunk.js"
+  },
+  {
+    "revision": "f1080b5d79f82c39f5a4",
+    "url": "/React-Youtube/static/js/runtime-main.8f0254fb.js"
+  },
+  {
+    "revision": "3fd532143f7e16cdd21d977be564f35a",
+    "url": "/React-Youtube/static/media/Logo.3fd53214.png"
+  }
+]);
